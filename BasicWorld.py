c49a105d166f17a3e677a39dda4ff3f5e25ac0e2
Robert Boyd III <rob@robertcboyd.com>
2019-07-09 14:58:50 -0400

    Refactor Worlds into modules

--- BasicWorld.py
+++ BasicWorld.py
@@ -1,26 +1,23 @@
 from World import Place
 from Item import Item
 
+key = Item("Key", "A simple worn metal key.")
+hill = Place("Hill", "You walk up a long path to the top of a nearby hill. The air is clear and there is a castle far to the north.", [key])
+grasslands = Place("", "You reach a grasslands area at the foot of a hill to the south. To the north there is a castle.")
 
-class BasicWorld:
-    def __init__(self):
-        self.key = Item("Key", "A simple worn metal key.")
-        self.hill = Place("Hill", "You walk up a long path to the top of a nearby hill. The air is clear and there is a castle far to the north.", [self.key])
-        self.grasslands = Place("", "You reach a grasslands area at the foot of a hill to the south. To the north there is a castle.")
+abandoned_castle_front = Place("Castle Front", "You are at the front of what appears to be a ruined castle. The castle has 4 major towers the nearest ones - those to the southeast and the southwest - are in tatters. The northern towers still appear relatively intact.")
+abandoned_castle_corner_se = Place("Castle SE tower", "A tower (in shambles) rises upon this corner of the castle.")
+abandoned_castle_corner_sw = Place("Castle SW tower", "A ruined tower rests above. Moss covers the stones still standing. A few large stones are scattered around the area.")
+swarshy_swamp = Place("Swarshy Swamp", "Muck and vines lather the faces of the trees.")
+granite_obelisk = Place("Obelisk of the Lathenheim Fiends", "A shiny black and red tower rises into the swirling purple sky.")
 
-        self.abandoned_castle_front = Place("Castle Front", "You are at the front of what appears to be a ruined castle. The castle has 4 major towers the nearest ones - those to the southeast and the southwest - are in tatters. The northern towers still appear relatively intact.")
-        self.abandoned_castle_corner_se = Place("Castle SE tower", "A tower (in shambles) rises upon this corner of the castle.")
-        self.abandoned_castle_corner_sw = Place("Castle SW tower", "A ruined tower rests above. Moss covers the stones still standing. A few large stones are scattered around the area.")
-        self.swarshy_swamp = Place("Swarshy Swamp", "Muck and vines lather the faces of the trees.")
-        self.granite_obelisk = Place("Obelisk of the Lathenheim Fiends", "A shiny black and red tower rises into the swirling purple sky.")
+# Set player's initial location
+inital_location = hill
 
-        # Set player's initial location
-        self.inital_location = self.hill
-
-        # Wire together all of the places
-        self.grasslands.attach_to_north_of(self.hill)
-        self.abandoned_castle_front.attach_to_north_of(self.grasslands)
-        self.abandoned_castle_corner_se.attach_to_east_of(self.abandoned_castle_front)
-        self.abandoned_castle_corner_sw.attach_to_west_of(self.abandoned_castle_front)
-        self.swarshy_swamp.attach_to_east_of(self.grasslands)
-        self.granite_obelisk.attach_to_east_of(self.swarshy_swamp)
+# Wire together all of the places
+grasslands.attach_to_north_of(hill)
+abandoned_castle_front.attach_to_north_of(grasslands)
+abandoned_castle_corner_se.attach_to_east_of(abandoned_castle_front)
+abandoned_castle_corner_sw.attach_to_west_of(abandoned_castle_front)
+swarshy_swamp.attach_to_east_of(grasslands)
+granite_obelisk.attach_to_east_of(swarshy_swamp)
--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 The main game class.
 """
 import UI
-from BasicWorld import BasicWorld
+import BasicWorld as world
 from Player import Player
 
+# TODO allow player to pick a world from many options
 
 def main():
     """
@@ -12,8 +13,6 @@ def main():
     """
     UI.println("Welcome to my game!")
 
-    # TODO allow player to pick a world from many options
-    world = BasicWorld()
     player = Player(100, [], world.inital_location)
     while True:
         player.location.on_enter(player)
