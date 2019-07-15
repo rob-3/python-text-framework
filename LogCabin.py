1bb6b334b743bb0e2d7789f8f9c2fd77b1929355
Garry <32161888a@gmail.com>
2019-07-15 14:22:41 -0400

    Begin to edit Log Cabin

--- LogCabin.py
+++ LogCabin.py
@@ -1,4 +1,11 @@
 
-from Place import Place
+from Place import Place, Door
 from Item import Item
 
+intro_text = 'You approach a worn down log cabin. The cabin appears to be shabby and of low quality, but the walls stand strong.'
+
+exterior = Place('Cabin Exterior', 'The ground around the cabin is muddy, but a gravel path leads to the front door of the cabin.')
+livingroom = Place('Cabin Living Room', 'In the main room of the log cabin, there is a lit fireplace that crackles as it gives off heat, and there are chairs in a circular pattern.')
+
+
+ 
