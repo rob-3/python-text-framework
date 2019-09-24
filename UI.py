b79e0042bb8dabfec753c37f1747f4b774e9adae
Robert Boyd III <rob@robertcboyd.com>
2019-09-24 14:56:27 -0400

    Refactor UI
    - No breaking changes
    - print_in_box throws exception if string is too long
    - Add docs
    - Add wrap_in_box() which is the replacement for print_in_box()
    - Deprecate print_in_box()

--- UI.py
+++ UI.py
@@ -4,20 +4,77 @@ Fully implemented
 import textwrap
 from builtins import print as builtinprint
 
-
 def println(string=''):
+    '''
+    Print string, wrapped at 80 chars. Adds a newline to the end of the string.
+    Use instead of print() to ensure wrapping works as intended.
+    '''
     builtinprint(textwrap.fill(string, 80))
 
 def print(string=''):
+    '''
+    Print string, wrapped at 80 chars. Does not add a newline to the end of the
+    string. You probably want UI.println().
+    '''
     builtinprint(textwrap.fill(string, 80), end='')
+
+class StringTooLongException(Exception):
+    '''
+    This exception should be raised when a string is too long for some reason
+    (eg box-drawing). It should not be raised in normal usage.
+    '''
+
+def wrap_in_box(string, heavy):
+    '''
+    Returns a string inside of a box using box drawing characters. Passing None
+    or '' will result in a return of '', with no box. There is a newline at the
+    end, leaving the string on the line following bottom of the box.
+    '''
+    if len(string) > 78:
+        raise StringTooLongException(string)
+    return_string = ''
+    if string != '' and string is not None:
+        if(heavy):
+            # top line of box index 1 to allow the different first character
+            return_string += '┏'
+            for char in string:
+                return_string += '━'
+            return_string += '┓\n'
+
+            # middle line of box
+            return_string += '┃' + string + '┃\n'
+
+            # bottom line of box index 1 to allow the different first character
+            return_string += '┗'
+            for char in string:
+                return_string += '━'
+            return_string += '┛\n'
+        else:
+            # top line of box index 1 to allow the different first character
+            return_string += '┌'
+            for char in string:
+                return_string += '─'
+            return_string += '┐\n'
+
+            # middle line of box
+            return_string += '│' + string + '│\n'
+
+            # bottom line of box index 1 to allow the different first character
+            return_string += '└'
+            for char in string:
+                return_string += '─'
+            return_string += '┘\n'
+    return return_string
 
 def print_in_box(string, heavy):
     '''
-    Prints a string inside of a box using box drawing characters. Passing None
-    or '' will result in a return without any box being printed.
+    DEPRECATED: Use UI.wrap_in_box() instead, then call UI.println(). Prints a
+    string inside of a box using box drawing characters.  Passing None or ''
+    will result in a return without any box being printed.
     '''
+    if len(string) > 78:
+        raise StringTooLongException(string)
     if string != '' and string is not None:
-        # FIXME don't have problems when the string is too long
         if(heavy):
             # top line of box index 1 to allow the different first character
             print('┏')
