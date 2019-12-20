a1f5c1cac0cd55f348acb6b158105569f4783dec
Robert Boyd III <rob@robertcboyd.com>
2019-12-20 13:44:00 -0500

    Rewrite LogCabin to fit new game format

--- LogCabin.py
+++ LogCabin.py
@@ -1,6 +1,8 @@
+import UI
 from Place import *
 from Item import *
 from Container import Container
+from Player import Player
 
 intro_text = 'You wake up feeling terrible. Your head is aching, and you are sitting against a wall with a broken window on it. You have multiple cuts and bruises on you, and you are very tired and injured. You see a man who you recognise to be your best friend laying on the ground with a pool of blood around him. To your horror, you realize that he is dead with a knife in his heart, and he too has multiple cuts and bruises on his body. The last thing you remember is your friend\'s horrified face as he fell to the ground, dying.'
 
@@ -25,4 +27,23 @@ cabin_livingroom.east = cabin_bedroom2
 cabin_bedroom2.north = cabin_closet
 
 initial_location = cabin_livingroom
-world_globals = globals()
+
+def start_adventure(player):
+    UI.println(intro_text)
+    player.globals = globals()
+
+    player.location = initial_location
+
+    while True:
+        player.location.on_enter(player)
+
+    # when done end adventure
+    end_adventure()
+
+def end_adventure():
+    '''
+    Clean up the game here, display any last messages, write to disk, etc
+    '''
+
+if __name__ == '__main__':
+    start_adventure(Player())
